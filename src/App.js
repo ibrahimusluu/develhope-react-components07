16d071ed60f3722546e2a87c83e9d1e21fa313ce
İbrahim Uslu <ibrahim069612@gmail.com>
2023-01-04 15:03:06 +0300

    extracted into Message File

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Hello from "Hello.js";
+import React from "react";
+
+export class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Hello />
+      </div>
+    );
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import ReactDOM from "react-dom"; // {ReactDOM} is wrong and cannot be recognized in this way, What is the difference with {} and without it?
+import { Hello } from "./Hello";
+
+ReactDOM.render(<Hello />, document.querySelector("#root"));
